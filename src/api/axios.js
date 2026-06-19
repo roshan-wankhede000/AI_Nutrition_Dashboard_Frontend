e5a11c1d61d36d30aaad6a52961a62a5d255af44
roshan-wankhede000 <roshanwankhde212@gmail.com>
2026-06-19 18:03:20 +0530

    backend exios path changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 //   baseURL: "http://localhost:5000/api"
-  baseURL: "https://ai-nutrition-dashboard-backend.onrender.com/api"
+  baseURL: "https://ai-nutrition-dashboard-backend.onrender.com"
 });
 
 API.interceptors.request.use(
