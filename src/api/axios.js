f2526a4c8405027e6961c33b54090aea9d5a1430
roshan-wankhede000 <roshanwankhde212@gmail.com>
2026-06-19 18:11:12 +0530

    baseurl changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 //   baseURL: "http://localhost:5000/api"
-  baseURL: "https://ai-nutrition-dashboard-backend.onrender.com"
+  baseURL: "https://ai-nutrition-dashboard-backend.onrender.com/api"
 });
 
 API.interceptors.request.use(
